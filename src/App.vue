15df7fb6837673114e8f97fd68081ae4103d5c0b
lehoangtu <tub2306648@student.ctu.edu.vn>
2026-07-10 11:02:41 +0700

    Khoi tao du an

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+}
+</script>
+
+<template>
+  <div class="page">
+    <h1>Hello, Vue.js!</h1>
+  </div>
+</template>
+
+<style>
+.page {
+  max-width: 400px; 
+  margin: auto;
+}
+</style>
